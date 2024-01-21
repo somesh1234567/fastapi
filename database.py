980cf6b1b097dde837662cd637cd3aebcd352def
Somesh Panigrahi <someshpanigrahi@192.168.1.2>
2024-01-21 12:02:24 +0530

    production database added

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = "postgresql://vpuqmlyz:irVQYNwn0fqOGweXvSBVNji0H-J3my4s@tiny.db.elephantsql.com/vpuqmlyz"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
